27e4003b198e8cbd63f16b8b5a427efe39d458df
Sophia Price <pricesophia.dev@gmail.com>
2019-01-05 23:36:45 +0000

    JSX

--- src/Pet.js
+++ src/Pet.js
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Pet extends Component {
-  render() {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "props.name"),
-      React.createElement("h2", {}, "props.animal"),
-      React.createElement("h2", {}, "props.breed")
-      // React.createElement("h1", {}, props.name),
-      // React.createElement("h2", {}, props.animal),
-      // React.createElement("h2", {}, props.breed)
-    ]);
-  }
-}
+const Pet = props => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
+};
 
 export default Pet;
