dc309358850c3e82613ef7060d81daec122121ea
Sophia Price <pricesophia.dev@gmail.com>
2019-01-06 15:43:25 +0000

    Rendering Data 1, display code in html

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends Component {
-  handleTitleClick() {
-    alert("You clicked the title");
+  state = {
+    pets: []
+  };
+
+  componentDidMount() {
+    petfinder.pet
+      .find({
+        output: "full",
+        location: "Seattle, WA"
+      })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
   }
 
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-        <Pet name="Doink" animal="cat" breed="Mixed" />
+        <pre>
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+        </pre>
       </div>
     );
   }
