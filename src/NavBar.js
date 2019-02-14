ab14ffa128d2fb88108f0f3ea955e8155bd5bf14
Sophia <sprice@overstock.com>
2019-02-14 15:50:23 +0000

    reusable styles, set global colors, NavLink style

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,34 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from "@emotion/styled";
+import colors from "./colors";
 
 const Header = styled.header`
-  background-color: #333;
+  background-color: ${colors.dark};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+
+  span {
+    display: inline-block;
+    border: 1px solid red;
+  }
+`;
+
 const NavBar = () => (
   <Header>
-    <Link to="/">Adopt Me!</Link>
-    <Link to="/search-params">
+    <NavLink to="/">Adopt Me!</NavLink>
+    <NavLink to="/search-params">
       <span aria-label="search" role="img">
         🔍
       </span>
-    </Link>
+    </NavLink>
   </Header>
 );
 
