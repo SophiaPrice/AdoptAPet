1532ac6c769cde0bf82625a5ea1796b72f8331bb
Sophia <sprice@overstock.com>
2019-02-14 16:03:34 +0000

    remove span style

--- src/NavBar.js
+++ src/NavBar.js
@@ -14,11 +14,6 @@ const NavLink = styled(Link)`
   &:hover {
     text-decoration: underline;
   }
-
-  span {
-    display: inline-block;
-    border: 1px solid red;
-  }
 `;
 
 const NavBar = () => (
