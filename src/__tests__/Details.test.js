e072dbd2b31d2cf7569befa426985cb9be8b8a0b
Sophia <sprice@overstock.com>
2019-02-14 14:57:04 +0000

    test component method, add jest --watch

--- src/__tests__/Details.test.js
+++ src/__tests__/Details.test.js
@@ -8,3 +8,15 @@ test("snapshot", () => {
   const c = create(<Details />);
   expect(c.toJSON()).toMatchSnapshot();
 });
+
+// test Component Method
+test("shows modal when toggleModal is called", () => {
+  const c = create(<Details search={() => {}} />);
+  const instance = c.getInstance();
+
+  expect(instance.state.showModal).toBe(false);
+
+  instance.toggleModal();
+
+  expect(instance.state.showModal).toBe(true);
+});
