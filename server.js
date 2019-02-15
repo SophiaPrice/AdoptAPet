4b52341d2e1a746012754377deef495f68ee3381
Sophia <sprice@overstock.com>
2019-02-15 15:47:36 +0000

    React 16 improved SSR

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import React from "react";
 import express from "express";
 import fs from "fs";
-import { renderToString } from "react-dom/server";
+import { renderToNodeStream } from "react-dom/server";
 import { ServerLocation } from "@reach/router";
 import App from "./src/App";
 
@@ -14,14 +14,25 @@ const parts = html.split("not rendered");
 const app = express();
 app.use("/dist", express.static("dist"));
 app.use((req, res) => {
+  res.write(parts[0]);
+
   const reactMarkup = (
     <ServerLocation url={req.url}>
       <App />
     </ServerLocation>
   );
 
-  res.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
-  res.end();
+  const stream = renderToNodeStream(reactMarkup);
+
+  stream.pipe(
+    res,
+    { end: false }
+  );
+
+  stream.on("end", () => {
+    res.write(parts[1]);
+    res.end();
+  });
 });
 
 console.log(`listening on ${PORT}`);
