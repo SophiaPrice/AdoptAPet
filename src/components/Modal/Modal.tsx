c2dae90f243eeb53cde1f0aaf77923b85dbbe48f
Sophia <sprice@overstock.com>
2019-02-18 11:12:42 +0000

    remove eslint, add tslint

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,19 +3,23 @@ import { Component } from "react";
 import { createPortal } from "react-dom";
 
 class Modal extends Component {
-  modalRoot = document.getElementById("modal");
-  el = document.createElement("div");
+  private modalRoot = document.getElementById("modal");
+  private el = document.createElement("div");
 
   public componentDidMount() {
-    if (this.modalRoot) this.modalRoot.appendChild(this.el);
+    if (this.modalRoot) {
+      this.modalRoot.appendChild(this.el);
+    }
   }
 
   public componentWillUnmount() {
     // Prevents memory leaks
-    if (this.modalRoot) this.modalRoot.removeChild(this.el);
+    if (this.modalRoot) {
+      this.modalRoot.removeChild(this.el);
+    }
   }
 
-  render() {
+  public render() {
     return createPortal(this.props.children, this.el);
   }
 }
